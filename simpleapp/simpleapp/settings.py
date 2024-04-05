369ac1392dfcd0d0ef569db376218da914aad3e5
buenkov <buenkov-ta@yandex.ru>
2024-04-05 13:59:22 +0300

    Homework 7

--- simpleapp/simpleapp/settings.py
+++ simpleapp/simpleapp/settings.py
@@ -169,7 +169,7 @@ APSCHEDULER_RUN_NOW_TIMEOUT = 25
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'buenkov-ta'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = '!PenelopA1!PenelopA1'  # пароль от почты
+EMAIL_HOST_PASSWORD = '*****'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 DEFAULT_FROM_EMAIL = 'buenkov-ta@yandex.ru'
